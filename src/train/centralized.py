4b791610b26460eb0cb998df067ec8317b7c0b66
supershadoe <shadoe@shadoe.dev>
2025-09-05 10:02:59 +0530

    Change dataset dir

--- src/train/centralized.py
+++ src/train/centralized.py
@@ -21,7 +21,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-PROCESSED_DATA_DIR = "dataset"
+PROCESSED_DATA_DIR = "../dataset"
 SUBJECTS_TO_USE = list(range(101, 109))
 RARE_ACTIVITIES = [9, 11, 20] # watching TV, car driving, playing soccer
 
